e1289f5baa576f8c159a0f8731afe58ef0e63e00
umangagarwalit <umangagarwalit@gmail.com>
2019-04-23 17:21:27 +0530

    mycommit

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,7 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.war
+java -jar target/${NAME}-${VERSION}.war &
+netstat -anp -t
+ps -eal
+
