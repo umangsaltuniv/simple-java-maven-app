1975b6216f8d1c8ec5469c73affe547a62521918
umangagarwalit <umangagarwalit@gmail.com>
2019-05-27 21:24:34 +0530

    mycommit

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-#java -jar target/${NAME}-${VERSION}.war &
-java -jar "tmp/apache-tomcat-9.0.20/webapps/${NAME}-${VERSION}.war" &
+java -jar target/${NAME}-${VERSION}.war &
